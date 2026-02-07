64d6a50cfbc1ba7ae13a4e94da559c5c98e48863
Dipak Joshi <joshidipak8010@gmail.com>
2026-02-07 11:12:47 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,29 @@
+a="dipak"
+for i in a:
+    print(i)
+
+list=['ram','sham','sai','hari']
+for i in list:
+    print(i)
+    for n in i:
+        print(n)
+
+n=8
+for i in range(1,11):
+    print(i,'*',n,'=',i*n)
+
+for k in range(1,6):
+    print(k)
+
+
+t="hello im coder"
+t=t.split(" ")
+for i in t:
+    print(i)
+    for k in i:
+        print(k)
+
+
+
+t=(1,2,3,4,5,6,7,8,9,10)
+print(t)
