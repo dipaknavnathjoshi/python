623360d51986b747907edc59535d2c2752269959
Dipak Joshi <joshidipak8010@gmail.com>
2026-02-09 11:03:34 +0530

    Add files via upload

---
+++ breakcontinew.py
@@ -0,0 +1,13 @@
+
+print("Break example:")
+for i in range(10):
+    if i == 5:
+        break
+    print(i)
+
+print("\nContinue example:")
+
+for i in range(10):
+    if i % 2 == 0+1:
+        continue
+    print(i)
