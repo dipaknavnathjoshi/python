0e9f6820c078c8e66b62c2cb4f6421d0d969938e
Dipak Joshi <joshidipak8010@gmail.com>
2026-02-17 13:52:22 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,8 @@
+import random
+ 
+print(random.random())
+print(random.randint(-100,100))
+print(random.randrange(10,120,1))
+dice=[1,2,3,4,5,6]
+print("Dice: ",random.choice(dice))
+print("Dice: ",random.randint(1,6))
