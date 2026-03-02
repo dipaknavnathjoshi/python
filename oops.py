4749dc60a6a12f231a1e1f96756251bb91031b02
Dipak Joshi <joshidipak8010@gmail.com>
2026-03-02 16:20:57 +0530

    Add files via upload

---
+++ oops.py
@@ -0,0 +1,21 @@
+# working with OOPs
+class person:
+    name = "dipak"
+    sir = "joshi"
+    age = 21
+    qualification = "BCA"
+    # cunstructor
+    def __init__(self,n):
+        print("hello I`m Human")
+        self.name=n
+        print("my name is",self.name)
+    # class method 
+   
+a=person("deep")
+n=person("gone")
+ 
+
+ 
+
+
+
